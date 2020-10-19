7a4063e64a8b3d1c4f7d2d6c68b86f33b60cf731
Joost Vermeulen <joost@jover.net>
2020-10-19 21:34:40 +0200

    WIP question / answer models and stuff

---
+++ database/migrations/2020_10_19_174606_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('question');
+            $table->integer('points')->default(0);
+            $table->enum('type', ['text', 'number', 'picture', 'youtube'])->default('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
